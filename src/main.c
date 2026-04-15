e3562502ed64919420b0046f548927ec2860845c
Murilo Rodrigues Gomes <rodriguesmurilo@usp.br>
2026-04-15 12:20:43 -0300

    motor_codigo

--- src/main.c
+++ src/main.c
@@ -3,36 +3,32 @@
 #include <zephyr/drivers/gpio.h>       // API para controle de pinos de entrada/saída (GPIO)
 #include <pwm_z42.h>                   // Biblioteca personalizada com funções de controle do TPM (Timer/PWM Module)
 
-#define TEMPO 1000 // 1 segundo
+#define TEMPO 2000 // 2 segundo
 // Define o valor do registrador MOD do TPM para configurar o período do PWM
 #define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
 // Valores de duty cycle correspondentes a diferentes larguras de pulso
-uint16_t duty_50  = TPM_MODULE*0.7;       
-uint16_t duty_25  = TPM_MODULE*0.9;       
+uint16_t duty_Red  = TPM_MODULE*0.500;             
 
 int main(void)
 {
-    // Inicializa o módulo TPM2 com:
-    // - base do TPMx
-    // - fonte de clock PLL/FLL (TPM_CLK)
-    // - valor do registrador MOD
-    // - tipo de clock (TPM_CLK)
-    // - prescaler de 1 a 128 (PS)
-    // - modo de operação EDGE_PWM
     pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
     // Inicializa o canal 0 do TPM2 para gerar sinal PWM na porta GPIOB_18
     // - modo TPM_PWM_H (nível alto durante o pulso)
     pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); // vermelho
     pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19); // Verde
-
+    //Pinos dos motores (PTB2 e PTB3)
+    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 12); //PTB2 - pino 12
+    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 13); //PTB3 - pino 13
     // Loop infinito
     while(1)
     {
-        pwm_tpm_CnV(TPM2, 0, duty_50);
-        pwm_tpm_CnV(TPM2, 1, duty_25);
-        
+        pwm_tpm_CnV(TPM2, 0, duty_Red);
+        pwm_tpm_CnV(TPM2, 1 , 1000);
+        k_msleep(TEMPO);
+        pwm_tpm_CnV(TPM2, 0, 1000);
+        pwm_tpm_CnV(TPM2, 1, duty_Red);
+        k_msleep(TEMPO);
     }
-
     return 0;
 }   
